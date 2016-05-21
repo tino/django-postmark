aa2fb4d01bc54d676ffdf025432ed4f8f8ba90eb
Tino de Bruijn <tinodb@gmail.com>
2016-05-21 20:16:21 +0300

    TZ info in isoformat can be separated by a + or -

--- postmark/models.py
+++ postmark/models.py
@@ -101,10 +101,15 @@ def sent_message(sender, **kwargs):
         
         if not recipient[0]:
             continue
-        
-        timestamp, tz = resp["SubmittedAt"].rsplit("+", 1)
+
+        try:
+            timestamp, tz = resp["SubmittedAt"].rsplit("+", 1)
+            tz_sign = "+"
+        except ValueError:
+            timestamp, tz = resp["SubmittedAt"].rsplit("-", 1)
+            tz_sign = "-"
         tz_offset = int(tz.split(":", 1)[0])
-        tz = timezone("Etc/GMT%s%d" % ("+" if tz_offset >= 0 else "-", tz_offset))
+        tz = timezone("Etc/GMT%s%d" % (tz_sign, tz_offset))
         submitted_at = tz.localize(datetime.strptime(timestamp[:26], POSTMARK_DATETIME_STRING)).astimezone(pytz.utc)
         
         
